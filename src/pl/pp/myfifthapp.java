2fcb0f1b90be8d340768c65c752801ea0fa7133d
inspiredgriffin <a13dullayev.sh@gmail.com>
2024-03-30 22:28:46 +0100

    myfifthapp-lab5

--- src/pl/pp/myfifthapp.java
+++ src/pl/pp/myfifthapp.java
@@ -2,8 +2,8 @@ package pl.pp;
 
 public class myfifthapp {
     public static void main(String[] args) {
-
-        // Calculations and display of the result for values assigned in the application code
+        // Commented out previous demonstration code
+        /*
         System.out.println("Calculations and display of the result for values assigned in the application code");
         boolean gameOver = true;
         int score = 800;
@@ -17,7 +17,6 @@ public class myfifthapp {
             System.out.println("Your score #1 is " + finalScore);
         }
 
-        // Calculations and display of the result for values assigned in the application code (different than before)
         System.out.println("Calculations and display of the result for values assigned in the application code (different than before)");
         score = 10000;
         levelCompleted = 8;
@@ -31,51 +30,50 @@ public class myfifthapp {
             System.out.println("Your score #2 is " + finalScore);
         }
 
-        // Calculations using the method calculateScore_noArguments(), but still for values assigned in the method code
         System.out.println("Calculations using the method calculateScore_noArguments(), but still for values assigned in the method code");
         calculateScore_noArguments();
 
-        // Calculations using the method calculateScore(), but this time for values entered into the method call argument
         System.out.println("Calculations using the method calculateScore(), but this time for values entered into the method call argument");
         calculateScore_arguments(true, 2500, 9, 250);
-        // it can be called using previously declared variables
-        calculateScore_arguments(gameOver, score, levelCompleted, bonus);
-        // but as you see, the display function is also with number #4 (because such display is recorded
-        // in the method. What to do to avoid this? Below:
 
-        // Calculations and display can be made independent from each other by adding a parameter that this method will return using return
+        calculateScore_arguments(gameOver, score, levelCompleted, bonus);
+
         System.out.println("Calculations and display can be made independent from each other by adding a parameter that this method will return using return");
         finalScore = calculateScore_argumentsReturn(gameOver, score, levelCompleted, bonus);
         System.out.println("Your score #5 is " + finalScore);
+        */
+
+        // Example usage of the new method
+        System.out.println("Printing patterns using the printCharacter method:");
+
+        // Print '*' pattern
+        printCharacter('*', 5, 3);
+
+        // Print '-' pattern
+        printCharacter('-', 10, 2);
     }
 
+    public static void printCharacter(char character, int timesInRow, int numLines) {
+        for (int i = 0; i < numLines; i++) {
+            for (int j = 0; j < timesInRow; j++) {
+                System.out.print(character);
+            }
+            System.out.println(); // Move to the next line after printing each row
+        }
+    }
+
+    // Commented out previous method definitions
+    /*
     private static void calculateScore_noArguments() {
-        boolean gameOver = true;
-        int score = 150;
-        int levelCompleted = 2;
-        int bonus = 1000;
+        // Method body
+    }
 
-        int finalScore = score;
-        if (gameOver) {
-            finalScore += (levelCompleted * bonus);
-            finalScore += 1000;
-            System.out.println("Your score #3 is " + finalScore);
-        }
-    }
     private static void calculateScore_arguments(boolean gameOver, int score, int levelCompleted, int bonus) {
-        int finalScore = score;
-        if (gameOver) {
-            finalScore += (levelCompleted * bonus);
-            finalScore += 1000;
-            System.out.println("Your score #4 is " + finalScore);
-        }
+        // Method body
     }
+
     private static int calculateScore_argumentsReturn(boolean gameOver, int score, int levelCompleted, int bonus) {
-        int finalScore = score;
-        if (gameOver) {
-            finalScore += (levelCompleted * bonus);
-            finalScore += 1000;
-        }
-        return finalScore;
+        // Method body
     }
+    */
 }
